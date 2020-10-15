bc2dc3a4ec669fb4009eb5a60834d10f8ddc5031
Fannie Ståhl <fannie.stahl@gmail.com>
2020-10-15 13:16:54 +0200

    Nyaste uppdateringarna

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
-// let startBtn = document.querySelector('.startBtn');
-// startBtn.addEventListener('click', startGame);
+//Adding the buttons from HTML
+let startBtn = document.querySelector('#startBtn');
+startBtn.addEventListener('click', startGame);
+
+let restartBtn = document.querySelector('#restartBtn');
+restartBtn.addEventListener('click', restartGame);
+
 let gamePaused = true;
 
 let bullets = [];
@@ -15,7 +20,10 @@ let timer = 0;
 let spawnTimer = 0;
 let coconut;
 let bullet;
-
+let defaultHealthWidth;
+let healthValue = 100;
+let calculatedX = 0;
+let score = 0;
 // !!!VERY IMPORTANT DO NOT REMOVE!!!
 let deltaTime = 0;
 
@@ -56,29 +64,52 @@ function preload() {
     this.load.image('bullet', 'images/banan2.png');
     this.load.audio('takingDamage', 'audio/monkey-cry.ogg');
     this.load.audio('destroy', 'audio/destroy.wav');
+    this.load.audio('donk', 'audio/donk.wav')
     this.load.image('coconut', 'images/coconut3.png');
 }
-
-
-function startGame() {
-    gamePaused = false;
-}
-
 
 // Creates all new sprites, audio and more
 function create() {
 
     start = new Date();
 
-    // Skapar Apan//
-    player = this.physics.add.sprite(window.innerWidth / 2, window.innerHeight / 1.18,  'player');
+    // Create Monkey and healthbar
+    player = this.physics.add.sprite(canvasX / 2, canvasY / 1.18, 'player');
+    health = this.add.sprite(player.x, player.y, 'health');
+    bar = this.add.sprite(health.x, health.y, 'bar');
+
+    scoreText = this.add.text(50, 50, 'Score: 0', {
+        fontFamily: 'Orbitron',
+        color: 'black',
+        fontSize: '1.5rem',
+        stroke: 'black',
+        strokeThickness: '1'
+    });
+
+    gameOverText = this.add.text(canvasX / 2, canvasY / 2, '', {
+        fontFamily: 'Orbitron',
+        color: 'black',
+        fontSize: '5rem',
+        stroke: 'black',
+        strokeThickness: '1',
+    });
+
+    gameOverText.setDepth(9000);
+    scoreText.setDepth(9000);
+
+
+    //Values for healthbar
+    defaultHealthWidth = health.displayWidth;
+
+
+    //Creating sounds
     takingDamage = this.sound.add('takingDamage');
     destroy = this.sound.add('destroy');
+    donk = this.sound.add('donk');
 
     destroy.allowMultiple = true;
-    takingDamage.allowMultiple = true;     
+    takingDamage.allowMultiple = true;
 }
-
 
 // Get how many seconds from 1 Jan 1970 00:00:00
 function getTime() {
@@ -96,25 +127,60 @@ function time() {
     return deltaTime;
 }
 
+function startGame() {
+    gamePaused = false;
+}
+
+function restartGame() {
+    score = 0;
+    scoreText.setText('Score: ' + score);
+    healthValue = 100;
+    health.x = player.x;
+    health.displayWidth = defaultHealthWidth;
+    health.setOrigin(0, 0);
+    gameOverText.setText('');
+
+    for (let j = 0; j < coconuts.length; j++) {
+        coconuts[j].destroy();
+    }
+    for (let j = 0; j < bullets.length; j++) {
+        bullets[j].destroy();
+    }
+    bullets = [];
+    coconuts = [];
+    spawnTimer = 1;
+
+    gamePaused = false;
+
+}
+
 // Updates every frame
 function update() {
-    if (gamePaused = true) {
-        deltaTime = time();
-
-        // Skapar en bullet med en timer som skjuts från apan//
+    deltaTime = time();
+    if (gamePaused == false) {
+        // Creates the bullets/bananas the monkey shoots
         if (game.input.activePointer.isDown && timer <= 0) {
             if (timer <= 0) {
                 timer = 0.2;
             }
-            bullet = this.add.sprite(player.x + 24 , player.y - 30, 'bullet');
+            bullet = this.add.sprite(player.x + 24, player.y - 30, 'bullet');
             bullets.push(bullet);
         }
 
-        
+
 
         // Updates Player Moverment
         player.x = this.input.mousePointer.x;
         player.y = window.innerHeight - (player.height);
+
+        //HealthBar position
+        bar.x = player.x;
+        bar.y = player.y;
+
+        health.x = player.x - calculatedX;
+        health.y = player.y;
+
+
 
 
         updateBullets();
@@ -135,63 +201,78 @@ function update() {
 }
 
 
-function checkCollision()
-{
-    if (bullets.length > 0 && coconuts.length > 0) 
-    {
+function checkCollision() {
+    //When bullet hit coconut
+    if (bullets.length > 0 && coconuts.length > 0) {
         for (let i = 0; i < bullets.length; i++) {
             for (let j = 0; j < coconuts.length; j++) {
-                if ( Phaser.Math.Distance.Between(bullets[i].x, bullets[i].y, coconuts[j].x, coconuts[j].y) < 26 )
-                {
+                if (Phaser.Math.Distance.Between(bullets[i].x, bullets[i].y, coconuts[j].x, coconuts[j].y) < 26) {
                     bullets[i].destroy();
                     bullets.splice(i, 1);
                     coconuts[j].destroy();
+                    donk.play();
                     coconuts.splice(j, 1);
+                    score += 10;
+                    scoreText.setText('Score: ' + score);
                     return true;
-                }               
+                }
             }
         }
 
-        
+
     }
-            for (let i = 0; i < coconuts.length; i++) {
-                if ( Phaser.Math.Distance.Between(player.x, player.y, coconuts[i].x, coconuts[i].y) < 75 )
-                {
-                    coconuts[i].destroy();
-                    coconuts.splice(i, 1);
-                    return true;
-                }               
+    //When player hit coconut
+    for (let i = 0; i < coconuts.length; i++) {
+        if (Phaser.Math.Distance.Between(player.x, player.y, coconuts[i].x, coconuts[i].y) < 75) {
+            coconuts[i].destroy();
+            coconuts.splice(i, 1);
+            takingDamage.play();
+            healthValue -= 20;
+            health.displayWidth = (healthValue / 100) * defaultHealthWidth;
+            calculatedX = (defaultHealthWidth - (defaultHealthWidth * (healthValue / 100))) / 2;
+            // GAME OVER
+            if (healthValue <= 0) {
+                gameOverText.setText('GAME OVER');
+                gameOverText.setPosition(canvasX / 2, canvasY / 2);
+                for (let j = 0; j < coconuts.length; j++) {
+                    coconuts[j].destroy();
+                }
+                for (let j = 0; j < bullets.length; j++) {
+                    bullets[j].destroy();
+                }
+                bullets = [];
+                coconuts = [];
+                gamePaused = true;
             }
+            return true;
+        }
+    }
 }
 
 // Updates Bullet logistics (position, isDead and more)
-function updateBullets()
-{
-    if (bullets.length > 0)
-     {
-         for (let i = 0; i < bullets.length; i++){
-             bullets[i].y -= bulletSpeed * deltaTime;
-     
-             if(bullets[i].y < 0)
-             {
-                 bullets[i].destroy();
-                 bullets.splice(i, 1);     
-             }
-         }
+function updateBullets() {
+    if (bullets.length > 0) {
+        for (let i = 0; i < bullets.length; i++) {
+            bullets[i].y -= bulletSpeed * deltaTime;
+
+            if (bullets[i].y < 0) {
+                bullets[i].destroy();
+                bullets.splice(i, 1);
+            }
+        }
     }
 }
 
 // Updates Coconuts logistics (position, isDead and more)
-function updateCoconuts()
-{
-    for (let i = 0; i < coconuts.length; i++)
-    {
+function updateCoconuts() {
+    for (let i = 0; i < coconuts.length; i++) {
         coconuts[i].y += coconutSpeed * deltaTime;
 
-        if(coconuts[i].y > window.innerHeight +50)
-        {
+        if (coconuts[i].y > window.innerHeight + 50) {
             coconuts[i].destroy();
             coconuts.splice(i, 1);
+            score -= 5;
+            scoreText.setText('Score: ' + score)
         }
     }
 }
@@ -210,11 +291,10 @@ function createPattern(create) {
     let randomX = 0;
 
     switch (randomName) {
-        case "arrow":   
+        case "arrow":
             coconut = create.add.sprite(canvasX / 2, 40, 'coconut');
             coconuts.push(coconut);
-            for(let i = 1; i < 8; i++)
-            {
+            for (let i = 1; i < 8; i++) {
                 coconut = create.add.sprite(canvasX / 2 + 75 * i, 40 - 75 * i, 'coconut');
                 coconuts.push(coconut);
                 coconut = create.add.sprite(canvasX / 2 - 75 * i, 40 - 75 * i, 'coconut');
